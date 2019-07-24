2dc7e333c89f9aa1ac295267e32a9a139414f47f
Cody Gillette <cgillette1312@gmail.com>
2019-07-24 15:39:51 -0700

    heroku

--- src/whitelist.js
+++ src/whitelist.js
@@ -1,4 +1,4 @@
-const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'https://noteful-app.cgillette12.now.sh'];
 const originGenerator = function (origin, callback) {
   if (whitelist.indexOf(origin) !== -1 || !origin) {
     callback(null, true);
